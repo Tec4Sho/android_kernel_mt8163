4d36bc74ce31f2f5f4d7644364b3b1ca3f8b0105
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2025-09-24 10:55:01 -0400

    Update gl_p2p_kal.c

--- drivers/misc/mediatek/connectivity/wlan/core/gen2/os/linux/gl_p2p_kal.c
+++ drivers/misc/mediatek/connectivity/wlan/core/gen2/os/linux/gl_p2p_kal.c
@@ -22,7 +22,7 @@
 #include "precomp.h"
 
 #ifndef CFG80211_VERSION_CODE‎
-#define CFG80211_VERSION_CODE‎  1
+#define CFG80211_VERSION_CODE‎
 #endif
 
 /*******************************************************************************
@@ -1634,6 +1634,7 @@ void kalP2pIndicateQueuedMgmtFrame(IN P_GLUE_INFO_T prGlueInfo,
 		prFrame->u2Length,
 		GFP_ATOMIC);
 }
+
 
 
 
