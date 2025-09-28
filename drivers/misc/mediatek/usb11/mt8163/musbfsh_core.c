587f251d062ea2816ec1f37d727c09356e5117f9
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2025-09-28 17:14:04 -0400

    Update musbfsh_core.c

--- drivers/misc/mediatek/usb11/mt8163/musbfsh_core.c
+++ drivers/misc/mediatek/usb11/mt8163/musbfsh_core.c
@@ -1250,7 +1250,6 @@ static int __init musbfsh_probe(struct platform_device *pdev)
 	unsigned long usb_mac_base;
 	unsigned long usb_phy11_base;
 	int retval = 0
-
 	INFO("[Flow][USB11]%s:%d, CONFIG_OF\n", __func__, __LINE__);
 #if 0
 	pr_info("musb probe\n");
