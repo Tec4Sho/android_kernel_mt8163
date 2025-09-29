d91f547922b4dd26606a1f745cde8c0728dd6afb
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2025-09-29 01:48:23 -0400

    Update musbfsh_debug.h

--- drivers/misc/mediatek/usb11/musbfsh_debug.h
+++ drivers/misc/mediatek/usb11/musbfsh_debug.h
@@ -17,14 +17,11 @@
 /* for normal log, very detail, impact performance a lot*/
 extern int musbfsh_debug;
 #define yprintk(facility, format, args...) \
-    do { if (musbfsh_debug) { \
-            printk(facility "[MUSBFSH] %s %d: " format , \
-            __func__, __LINE__ , ## args); } \
-    } while (0)
+        do { printk(facility "[MUSBFSH] %s %d: " format , \
+        __func__, __LINE__ , ## args); } while (0)
 #define INFO(fmt, args...) yprintk(KERN_NOTICE, fmt, ## args)
 /* for critical log */
 #define zprintk(facility, format, args...) printk(facility "[MUSBFSH] %s %d: " format , __func__, __LINE__ , ## args)
-
 #define WARNING(fmt, args...) zprintk(KERN_WARNING, fmt, ## args)
 #define ERR(fmt, args...) zprintk(KERN_ERR, fmt, ## args)
 extern int musbfsh_init_debugfs(struct musbfsh *musb);
