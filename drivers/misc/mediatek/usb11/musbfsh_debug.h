a8b9cdfd26e8c3f9de49495aaf0a550866db8b83
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2025-09-28 20:04:32 -0400

    Update musbfsh_debug.h

--- drivers/misc/mediatek/usb11/musbfsh_debug.h
+++ drivers/misc/mediatek/usb11/musbfsh_debug.h
@@ -16,7 +16,7 @@
 
 /* for normal log, very detail, impact performance a lot*/
 extern int musbfsh_debug;
-#define yprintk(facility, format, args...) \
+#define yprintk(facility, format, args) \
     do { \
         if (musbfsh_debug) { \
             printk(facility "[MUSBFSH] %s %d: " format, \
@@ -24,13 +24,13 @@ extern int musbfsh_debug;
         } \
     } while (0)
 
-#define INFO(fmt, args...) yprintk(KERN_NOTICE, fmt, ## args)
+#define INFO(fmt, args) yprintk(KERN_NOTICE, fmt, ## args)
 
 /* for critical log */
-#define zprintk(facility, format, args...) printk(facility "[MUSBFSH] %s %d: " format, __func__, __LINE__, ## args)
+#define zprintk(facility, format, args) printk(facility "[MUSBFSH] %s %d: " format, __func__, __LINE__, ## args)
 
-#define WARNING(fmt, args...) zprintk(KERN_WARNING, fmt, ## args)
-#define ERR(fmt, args...) zprintk(KERN_ERR, fmt, ## args)
+#define WARNING(fmt, args) zprintk(KERN_WARNING, fmt, ## args)
+#define ERR(fmt, args) zprintk(KERN_ERR, fmt, ## args)
 extern int musbfsh_init_debugfs(struct musbfsh *musb);
 extern void musbfsh_exit_debugfs(struct musbfsh *musb);
 
