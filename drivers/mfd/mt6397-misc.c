82bfc3558c3b92669e88a56de9e2bcb4ecf4c5f8
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2025-10-09 23:14:13 -0400

    Update mt6397-misc.c

--- drivers/mfd/mt6397-misc.c
+++ drivers/mfd/mt6397-misc.c
@@ -29,6 +29,10 @@
 #include <linux/notifier.h>
 #include <linux/kdebug.h>
 
+#ifndef CONFIG_MTK_RTC
+#include <mt-plat/mtk_rtc.h>
+#endif
+
 #define RTC_BBPU		0x0000
 #define RTC_BBPU_CBUSY		BIT(6)
 
