23806a50e28dac5d89d3ad57c9cd745fdfce4c90
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2025-10-11 19:49:23 -0400

    Update rtc_misc.h

--- include/linux/mfd/mt6397/rtc_misc.h
+++ include/linux/mfd/mt6397/rtc_misc.h
@@ -51,5 +51,56 @@ extern void rtc_gpio_disable_32k(u16 user);
 #define rtc_gpio_enable_32k(user)		do {} while (0)
 #define rtc_gpio_disable_32k(user)		do {} while (0)
 #endif
+#ifndef CONFIG_MTK_RTC
+extern void rtc_mark_fast(void);
+extern void rtc_mark_clear_lprst(void);
+extern void rtc_mark_enter_lprst(void);
+extern void rtc_mark_enter_sw_lprst(void);
+extern u16 rtc_rdwr_uart_bits(u16 *val);
+extern void rtc_bbpu_power_down(void);
+extern void rtc_read_pwron_alarm(struct rtc_wkalrm *alm);
+extern int get_rtc_spare_fg_value(void);
+extern int set_rtc_spare_fg_value(int val);
+extern void rtc_irq_handler(void);
+extern bool crystal_exist_status(void);
+extern bool rtc_lprst_detected(void);
+
+#ifndef rtc_mark_fast
+#define rtc_mark_fast()  ({ 0; })
+#endif
+#ifndef rtc_mark_clear_lprst
+#define rtc_mark_clear_lprst()  ({ 0; })
+#endif
+#ifndef rtc_mark_enter_lprst
+#define rtc_mark_enter_lprst()  ({ 0; })
+#endif
+#ifndef rtc_mark_enter_sw_lprst
+#define rtc_mark_enter_sw_lprst()  ({ 0; })
+#endif
+#ifndef rtc_rdwr_uart_bits
+#define rtc_rdwr_uart_bits(val)  ({ 0; })
+#endif
+#ifndef rtc_bbpu_power_down
+#define rtc_bbpu_power_down()  ({ 0; })
+#endif
+#ifndef rtc_read_pwron_alarm
+#define rtc_read_pwron_alarm(alm)  ({ 0; })
+#endif
+#ifndef get_rtc_spare_fg_value
+#define get_rtc_spare_fg_value()  ({ 0; })
+#endif
+#ifndef set_rtc_spare_fg_value
+#define set_rtc_spare_fg_value(val)  ({ 0; })
+#endif
+#ifndef rtc_irq_handler
+#define rtc_irq_handler()  ({ 0; })
+#endif
+#ifndef crystal_exist_status
+#define crystal_exist_status()  ({ 0; })
+#endif
+#ifndef rtc_lprst_detected
+#define rtc_lprst_detected()  ({ 0; })
+#endif
+#endif
 #endif /* __MT6397_RTC_MISC_H__ */
 
