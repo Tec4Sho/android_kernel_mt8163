ffc14ddb42942c16a703ece3bc54fa35bde891aa
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2025-10-18 00:24:48 -0400

    Update musbfsh_core.c

--- drivers/misc/mediatek/usb11/mt8163/musbfsh_core.c
+++ drivers/misc/mediatek/usb11/mt8163/musbfsh_core.c
@@ -1518,7 +1518,7 @@ static const struct dev_pm_ops musbfsh_dev_pm_ops = {
 #define	MUSBFSH_DEV_PM_OPS	NULL
 #endif
 
-static struct platform_driver musbfsh_driver __init = {
+static struct platform_driver musbfsh_driver = {
 	.driver = {
 		   .name = (char *)musbfsh_driver_name,
 		   .bus = &platform_bus_type,
@@ -1526,7 +1526,7 @@ static struct platform_driver musbfsh_driver __init = {
 		   .owner = THIS_MODULE,
 		   .pm = MUSBFSH_DEV_PM_OPS,
 		   },
-	.probe = musbfsh_probe,
+	.probe = __initdata(musbfsh_probe),
 	.remove = __exit_p(musbfsh_remove),
 	.shutdown = musbfsh_shutdown,
 };
