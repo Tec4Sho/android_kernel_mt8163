4032b72ed6897813d60dea2c7d5ae7871e1a70b1
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2026-03-07 17:58:19 -0500

    Add support for ARCH_MT8163 in LCM driver

--- drivers/misc/mediatek/lcm/CPT_CLAP070WP03XG_LVDS/lcm_drv.h
+++ drivers/misc/mediatek/lcm/CPT_CLAP070WP03XG_LVDS/lcm_drv.h
@@ -664,7 +664,8 @@ typedef struct {
 #if	defined(CONFIG_ARCH_MT6735) ||\
 	defined(CONFIG_ARCH_MT6735M) ||\
 	defined(CONFIG_ARCH_MT6753) ||\
-	defined(CONFIG_ARCH_MT6580)
+	defined(CONFIG_ARCH_MT6580) ||\
+    defined(CONFIG_ARCH_MT8163)
 extern LCM_DRIVER *lcm_driver_list[];
 extern unsigned int lcm_count;
 #endif
