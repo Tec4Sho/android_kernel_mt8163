ec161404afb309f2323a7d7c4c6a38a5206cddaa
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2026-03-08 12:39:56 -0400

    Remove unused hwPowerOn and hwPowerDown functions
    Removed unused power management functions.

--- drivers/misc/mediatek/lcm/CPT_CLAP070WP03XG_LVDS/CPT_CLAP070WP03XG_LVDS.c
+++ drivers/misc/mediatek/lcm/CPT_CLAP070WP03XG_LVDS/CPT_CLAP070WP03XG_LVDS.c
@@ -116,8 +116,6 @@ static LCM_UTIL_FUNCS lcm_util = {0};
 // ---------------------------------------------------------------------------
 //  Local Functions
 // ---------------------------------------------------------------------------
-void hwPowerOn(int type, int level, char* tag) {}
-void hwPowerDown(int type, char* tag) {}
 
 static __inline void send_ctrl_cmd(unsigned int cmd)
 {
